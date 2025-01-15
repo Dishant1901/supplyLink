79da92a12d48601243a248dc1029997454a728d0
username <wecp-dummy@gmail.com>
2025-01-15 11:34:24 +0000

    Day 16 passed - Angular types class

--- frontend/src/app/supplylink/types/Supplier.ts
+++ frontend/src/app/supplylink/types/Supplier.ts
@@ -1,10 +1,36 @@
 export class Supplier {
   
+    supllierId: number
+    supplierName: string
+    email: string
+    phone: string
+    address: string
+    username:string
+    password:string
+    role?: string
 
+    constructor(supplierId: number, supplierName: string, email:string, phone:string, address:string, username:string, password: string, role?:string ){
+        this.supllierId = supplierId
+        this.supplierName = supplierName
+        this.email = email
+        this.phone = phone
+        this.address = address
+        this.username= username
+        this.password = password
+        this.role = role
+    }
+    displayInfo() {
+        // console.log(`Supplier ID: ${this.supllierId} \nSupplier Name: ${this.supplierName} \nEmail: ${this.email}`)
+        console.log(`Supplier ID: ${this.supllierId}`)
+        console.log(`Supplier name: ${this.supplierName}`)
+        console.log(`email: ${this.email}`)
+   
+    }
    
 }
 
-
+// const supplier = new Supplier(1,"John Wane","johnwane@gmail.com","9876543210","texas","user1","sdkfjsdoifj")
+// supplier.displayInfo()
 
 
 
--- frontend/src/app/supplylink/types/Warehouse.ts
+++ frontend/src/app/supplylink/types/Warehouse.ts
@@ -1,4 +1,27 @@
 import { Supplier } from "./Supplier";
 export class Warehouse {
- 
+    warehouseId: number
+    supplierId:string
+    warehouseName:string
+    location: string
+    capacity:number
+
+    constructor(warehouseId:number, supplierId:string, warehouseName: string, location:string, capacity:number){
+        this.warehouseId=warehouseId
+        this.supplierId = supplierId
+        this.warehouseName = warehouseName
+        this.location = location
+        this.capacity = capacity
+    }
+
+    displayInfo(){
+        // console.log(`Warehouse ID: ${this.warehouseId} \nSupplier ID: ${this.supplierId} \nCapacity: ${this.capacity}`)
+    console.log(`Warehouse ID: ${this.warehouseId}`)
+    console.log(`Supplier ID: ${this.supplierId}`)
+    console.log(`Capacity: ${this.capacity}`)
+    
+    }
 }
+
+// const warehouse = new Warehouse(1,"1","ware1","texas",2)
+// warehouse.displayInfo()
